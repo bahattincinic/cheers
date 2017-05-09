34ca2d4a60673ffeb0b6ec7c1a304d7a6e08a62f
Bahattin Cinic <bahattin@brickly.io>
2017-05-09 21:58:49 +0300

    deployment changes

--- cheers/settings/prod.py
+++ cheers/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 
 
 ALLOWED_HOSTS = ['*']
@@ -6,3 +7,6 @@ ALLOWED_HOSTS = ['*']
 DEBUG = False
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)

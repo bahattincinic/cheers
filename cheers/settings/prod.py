b560c6f7552d9f53da1d186c9b44177f3a887e3c
Bahattin Cinic <bahattin@brickly.io>
2017-05-09 22:09:50 +0300

    config changes

--- cheers/settings/prod.py
+++ cheers/settings/prod.py
@@ -8,5 +8,13 @@ DEBUG = False
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+] + MIDDLEWARE
+
+INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
+] + INSTALLED_APPS
+
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)

fe51e9c682d837be75246bae8d93dcb57cfd729e
Bahattin Cinic <bahattin@brickly.io>
2017-05-11 21:17:01 +0300

    refactor

--- cheers/settings/base.py
+++ cheers/settings/base.py
@@ -1,9 +1,14 @@
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 PROJECT_ROOT = os.path.join(BASE_DIR, 'cheers')
+APPS = os.path.join(BASE_DIR, 'apps')
+
+sys.path.insert(1, APPS)
+sys.path.insert(2, PROJECT_ROOT)
 
 
 # Quick-start development settings - unsuitable for production

004f2086a4172bbd6f3fb22c5594e6d2ffe3448c
Bahattin Cinic <bahattin@brickly.io>
2017-05-14 01:20:06 +0300

    read only report json fields

--- apps/report/admin.py
+++ apps/report/admin.py
@@ -5,6 +5,11 @@ from .models import Report
 
 class ReportAdmin(admin.ModelAdmin):
     list_display = ('id', 'created_by', 'is_completed', 'created_at',)
+    readonly_fields = ('criterion_priority',
+                       'criterion_supplier_score',
+                       'criterion_compare',
+                       'criterions',
+                       'suppliers',)
 
 
 admin.site.register(Report, ReportAdmin)

3c3f43d7421113c7be89e80256e60949f1d5704a
Bahattin Cinic <bahattin@brickly.io>
2017-05-19 22:55:03 +0300

    add report detail view

--- apps/report/urls.py
+++ apps/report/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from .views import ReportListView
+from .views import ReportListView, ReportDetailView
 
 
 urlpatterns = [
     url(r'^$', ReportListView.as_view(), name="reports"),
+    url(r'^(?P<pk>[0-9]+)/$', ReportDetailView.as_view(),
+        name="report-detail"),
 ]
--- apps/report/views.py
+++ apps/report/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Report
 
@@ -8,3 +9,8 @@ class ReportListView(LoginRequiredMixin, ListView):
     queryset = Report.objects.filter(is_completed=True).order_by('-id')
     template_name = 'report/list.html'
     paginate_by = 10
+
+
+class ReportDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'report/detail.html'
+    queryset = Report.objects.filter(is_completed=True)
